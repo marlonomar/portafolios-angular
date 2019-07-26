e7f73ecf8156a35316b3454280b20c1e41caec42
Garcia <marlon.omar.g.garcia@accenture.com>
2019-07-26 17:29:36 -0300

    pagina no encontrada : cambio del contenido fijo

--- portafolios-angular/src/app/components/error/error.component.ts
+++ portafolios-angular/src/app/components/error/error.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error.component.scss']
 })
 export class ErrorComponent implements OnInit {
-
-  constructor() { }
+  public mesnajeError : String;
+  public image : String;
+  constructor() {
+    this.mesnajeError = 'Error 404 page not found...';
+    this.image = 'http://66.media.tumblr.com/tumblr_mdga1cKpK51rnqolfo1_500.gif';
+  }
 
   ngOnInit() {
   }
